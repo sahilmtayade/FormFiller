4cb5dc9a938c4b99350ad3386e6a243ea71db386
Sahil Tayade <sahilmtayade@gmail.com>
2025-01-01 12:16:05 -0500

    added support for nec but user still needs to customize the fieldmapping_nec yaml file to match their input csv.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import yaml
 from utils.fill_form import fill_form
 
 
-def read_yml() -> dict:
-    with open("field_number_mapping.yml", "r") as file:
+def read_yml(mapping_path="field_number_mapping.yml") -> dict:
+    with open(mapping_path, "r") as file:
         data = yaml.safe_load(file)
     return data
 
@@ -21,8 +21,14 @@ def read_csv(input_file: str) -> list[list]:
         return list(reader)
 
 
-def main(input_csv_file: str, template_path: str, one_doc=False):
-    field_mappings = read_yml()
+def main(
+    input_csv_file: str,
+    template_path: str,
+    one_doc=False,
+    output_big_file="big",
+    fm_path=None,
+):
+    field_mappings = read_yml(mapping_path=fm_path)
     csv_data = read_csv(input_file=input_csv_file)
     if one_doc:
         big_doc = fitz.open()
@@ -43,7 +49,7 @@ def main(input_csv_file: str, template_path: str, one_doc=False):
             # flatten=flatten,
             new_doc=big_doc,
         )
-    big_output_path = "outputs/big/big.pdf"
+    big_output_path = f"outputs/big/{output_big_file}.pdf"
     output_dir = os.path.dirname(big_output_path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)  # Create the output directory if it doesn't exist
@@ -59,6 +65,13 @@ if __name__ == "__main__":
         type=str,
         help="Name of the input file located in the 'inputs' folder.",
     )
+    parser.add_argument(
+        "--template",
+        "-t",
+        type=str,
+        default="misc",
+        help="Specify 'nec', 'misc', or a valid file path",
+    )
     args = parser.parse_args()
 
     # Join 'inputs' folder path with the provided file name
@@ -72,7 +85,25 @@ if __name__ == "__main__":
 
     # input_csv_file = "inputs/Tax 1099 print options MISC.csv"
     # input_csv_file = "inputs/example_input.csv"
-    template_path = "templates/1099_page_3.pdf"
+    if args.template == "misc":
+        template_path = "templates/1099_page_3.pdf"
+        output_big = "misc_big"
+        mapping_path = "field_number_mapping.yml"
+    if args.template == "nec":
+        template_path = "templates/nec_page_3.pdf"
+        output_big = "nec_big"
+        mapping_path = "field_number_mapping_nec.yml"
+    else:
+        template_path = args.template
+        output_big = "big"
+        mapping_path = "field_number_mapping_custom.yml"
+
     # flatten = True
     one_doc = True
-    main(input_csv_file=input_file_path, template_path=template_path, one_doc=one_doc)
+    main(
+        input_csv_file=input_file_path,
+        template_path=template_path,
+        one_doc=one_doc,
+        output_big_file=output_big,
+        fm_path=mapping_path,
+    )
